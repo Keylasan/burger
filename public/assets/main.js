737b4a68163af401f843575d4ce1628098da4832
Life is Now <keylasan28@yahoo.com>
2020-04-01 18:12:34 -0400

    main

--- public/assets/main.js
+++ public/assets/main.js
@@ -1,53 +1,40 @@
-// Make sure we wait to attach our handlers until the DOM is fully loaded.
-$(function() {
-  $(".change-sleep").on("click", function(event) {
-    var id = $(this).data("id");
-    var newSleep = $(this).data("newsleep");
-    var newSleepState = {
-      sleepy: newSleep
-    };
-    // Send the PUT request.
-    $.ajax("/api/cats/" + id, {
-      type: "PUT",
-      data: newSleepState
-    }).then(
-      function() {
-        console.log("changed sleep to", newSleep);
-        // Reload the page to get the updated list
-        location.reload();
-      }
-    );
-  });
-  $(".create-form").on("submit", function(event) {
-    // Make sure to preventDefault on a submit event.
+
+$(function () {
+  $(".create-form").on("submit", function (event) {
     event.preventDefault();
-    var newCat = {
-      name: $("#ca").val().trim(),
-      sleepy: $("[name=sleepy]:checked").val().trim()
+    //New Burger
+    var newBurger = {
+      burger_name: $("#submit-button").val().trim(),
     };
+ 
     // Send the POST request.
-    $.ajax("/api/cats", {
+    $.ajax("/api/burgers", {
       type: "POST",
-      data: newCat
+      data: newBurger
     }).then(
-      function() {
-        console.log("created new cat");
+      function () {
+        console.log("New burger ordered");
         // Reload the page to get the updated list
         location.reload();
       }
     );
   });
-  $(".delete-cat").on("click", function(event) {
+ 
+ 
+ 
+  $(".status-btn").on("click", function (event) {
     var id = $(this).data("id");
-    // Send the DELETE request.
-    $.ajax("/api/cats/" + id, {
-      type: "DELETE"
-    }).then(
-      function() {
-        console.log("deleted cat", id);
-        // Reload the page to get the updated list
-        location.reload();
-      }
-    );
+    var devoured = $(this).data("status");
+    var newBurgerState = {
+      newStatus: !devoured
+    };
+    $.ajax("api/burgers/" + id, {
+      type: "PUT",
+      data: newBurgerState
+    }).then(function () {
+      console.log("ID: " + id + " burger status changed to " + !devoured);
+      // Reload the page to get the updated list
+      location.reload();
+    });
   });
 });
